dfe78c95c7a0c3c41e5cd0a4dc802c0b5275545d
Jin2007 <jin2007@ukr.net>
2016-04-02 12:41:38 +0300

    Added files via upload

---
+++ Brick.java
@@ -0,0 +1,13 @@
+package Quadrants;
+
+import java.awt.Color;
+
+
+public class Brick extends AbstractTexture {
+
+	public Brick(int x, int y){
+		this.x = x;
+		this.y = y;
+		tColor = new Color(200,0,0);
+	}
+}
